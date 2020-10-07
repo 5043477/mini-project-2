05164e4ee0a196cb7cabc0cbc67023b871ae05ea
bwhitmore7 <bryson.whitmore@patelhs.org>
2020-10-07 20:23:38 +0000

    y

--- Main.java
+++ Main.java
@@ -1,7 +1,33 @@
-public class main
-{ 
-  public static void main(String[] args)
-  {
+public class LoopTest1
+{
+   public static void main(String[] args)
+   {
+     
+     int count = 2;
+
+     // 2. test the loop variable
+     while (count <= 14)
+     {
+        System.out.println(count);
+        // 3. change the loop variable
+        count++;
+     }
+    int x = 2;
+     if (x < 0)
+     {
+       System.out.println("x is negative");
+     }
+     else if (x == 0)
+     {
+        System.out.println("x is 0");
+     }
+     else
+     {
+       System.out.println("x is positive");
+     }
+     System.out.println("after conditional");
     
-  }
+     
+
+   }
 }
