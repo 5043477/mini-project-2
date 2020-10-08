d1249a1dca0cd77c1bac68992aefdb3dc62a6dec
bwhitmore7 <bryson.whitmore@patelhs.org>
2020-10-08 18:07:12 +0000

    y

--- Main.java
+++ Main.java
@@ -1,15 +1,15 @@
-public class LoopTest1
+public class Main
 {
    public static void main(String[] args)
    {
      
-     int count = 2;
+     int count = 1;
 
-     // 2. test the loop variable
+     
      while (count <= 14)
      {
         System.out.println(count);
-        // 3. change the loop variable
+        
         count++;
      }
     int x = 2;
@@ -25,9 +25,18 @@ public class LoopTest1
      {
        System.out.println("x is positive");
      }
-     System.out.println("after conditional");
-    
-     
+    boolean homeWork = true;
+     boolean playedVideoGames = false;
+     if (homeWork && playedVideoGames)
+     {
+         System.out.println("No, you can't play video games");
+     }
+     else
+     {
+         System.out.println("You can play video games.");
+         System.out.println("after condition of the homework being done.");
+        if
+     }
 
    }
 }
