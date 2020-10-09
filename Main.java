76a8cdd84018fccf38f9d8a585280c51c6828aad
bwhitmore7 <bryson.whitmore@patelhs.org>
2020-10-09 23:04:05 +0000

    i changed the thing to where it will have different activities to where i can play with my friends after i do my homework and get my grades up

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main
    public static void main(String[] args)
    {
      
-     int count = 1;
+     int count = 14;
 
      
      while (count <= 14)
@@ -17,7 +17,7 @@ public class Main
      {
        System.out.println("x is negative");
      }
-     else if (x == 0)
+     else if(x == 0)
      {
         System.out.println("x is 0");
      }
@@ -25,17 +25,23 @@ public class Main
      {
        System.out.println("x is positive");
      }
-    boolean homeWork = true;
+    boolean gradesBroughtUp = true;
      boolean playedVideoGames = false;
-     if (homeWork && playedVideoGames)
+     if (gradesBroughtUp && playedVideoGames)
      {
          System.out.println("No, you can't play video games");
      }
      else
      {
-         System.out.println("You can play video games.");
-         System.out.println("after condition of the homework being done.");
-        if
+         System.out.println("After the condition of the grades being brought up and homework being done, you can play video games.");  
+       boolean ageIs=true;
+       boolean mathGrade=false;
+       if( ageIs || mathGrade );
+       System.out.println("If your age is 14 or up you can play the video games or if your math grade is brought up to over a 70%, you can play video games");
+       boolean haveFiveFriends=true;
+       boolean haveFiveControllers=true;
+       if (haveFiveFriends && !haveFiveControllers);
+       System.out.println("you may have five friends over after you get four more controllers or else you can't play together on the two television screens.");
      }
 
    }
