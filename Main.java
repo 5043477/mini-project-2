b0ccb7fe77cb1ec0523cc3c11d2b61d71d8a564a
bwhitmore7 <bryson.whitmore@patelhs.org>
2020-10-14 21:19:43 +0000

    y

--- Main.java
+++ Main.java
@@ -1,32 +1,24 @@
 import java.util.Scanner;
-
+// impot this to help scanner
 
 public class Main
 {
    public static void main(String[] args)
    {
-
-     int count = 14;
-
-     
-     while (count <= 14)
+     // you start of with public class main and then you do public static void main(string[] args) to start off your program
+     Scanner scan = new Scanner(System.in);
+//if you are adding a scanner these are the steps to doing a scanner on your program.
+ System.out.println("whats your name?");
+ System.out.println("Bryson Whitmore");
+     System.out.println("Whats your age?");
+     System.out.println("14");
+   System.out.println("what's your senior quote");
+   System.out.println("Get good Grades to have a Good Life.");
+   System.out.println("Thank you"+ " Bryson Whitmore, " + "you are now" + " 14 " + "years old");
+   System.out.println("and your senior qoute is" + " Get good Grades to have a Good Life. ");
+    
      {
-        System.out.println(count);
-        
-        count++;
-     }
-    int x = 2;
-     if (x < 0)
-     {
-       System.out.println("x is negative");
-     }
-     else if(x == 0)
-     {
-        System.out.println("x is 0");
-     }
-     else
-     {
-       System.out.println("x is positive");
+       System.out.println("x is the number of controllers you have");
      }
     boolean gradesBroughtUp = true;
      boolean playedVideoGames = false;
@@ -40,26 +32,16 @@ public class Main
        boolean ageIs=true;
        boolean mathGrade=false;
        if( ageIs || mathGrade );
-       System.out.println("If your age is 14 or up you can play the video games or if your math grade is brought up to over a 70%, so now you can play video games");
-       System.out.println("After the condition of the grades being brought up and homework being done, you can play video games.");
+       System.out.println("If your age is 14 or up you can play the video games or if your math grade is brought up to over a 70%, you can play the video games");
        boolean haveFiveFriends=true;
        boolean haveFiveControllers=true;
        if (haveFiveFriends && !haveFiveControllers);
-       System.out.println("you may have five friends over after you get four more controllers for the playstation or whatever you call it, a ps4, or else you can't play together on the two television screens.");
-       Scanner scan = new Scanner(System.in);
+       System.out.println("you may have five friends over after you get four more controllers for the playstation or whatever you call it, a ps4, or else you can't play together on the two television screens in the different rooms.");
 
-   System.out.println("whats your name?");
-   String name = scan.next();
-   System.out.println(name);
-   System.out.println("Whats your age?");
-   int age = scan.nextInt();
-   System.out.println("what's your senior quote");
-   String quote = scan.next();
-    quote += scan.next();
-   System.out.println("Thank you"+ name + "you are" + age + "years old");
-   System.out.println("and your senior qoute is" + quote);
-  
+
+
      }
 
-   }
+  }
 }
+
